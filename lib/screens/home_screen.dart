b06baa3b371013cc413b4bebd1c0847141312323
Amod Mandal <mandala573@gmail.com>
2023-07-07 09:53:44 +0545

    HomeScreen implemented

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:onlinestore/constants.dart';
+import 'package:onlinestore/widgets/feeds_widget.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({Key? key}) : super(key: key);
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: kSpiritedGreen,
+      appBar: AppBar(
+        backgroundColor: kDarkGreenColor,
+        title: const Center(
+          child: Text(
+            'Home',
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(IconlyLight.search),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'All products',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Homescreen();
+                    },
+                    child: Text(
+                      'View all',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 8,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 8.0,
+                            mainAxisSpacing: 8.0,
+                            childAspectRatio: 0.8),
+                        itemBuilder: (ctx, index) {
+                          return const FeedsWidget();
+                        }),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/feeds_widget.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class FeedsWidget extends StatelessWidget {
+  const FeedsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Theme.of(context).cardColor,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(8.0),
+          onTap: () {},
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5, top: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: RichText(
+                        text: TextSpan(
+                            text: '\$',
+                            style: const TextStyle(
+                                color: Color.fromRGBO(33, 150, 243, 1)),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: "168.00",
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.w600)),
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 5),
+
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                    
+                    width: double.infinity,
+                    child: Image.network(
+                        'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png')),
+              ),
+              // ClipRRect(
+              //   borderRadius: BorderRadius.circular(12),
+              //   child: FancyShimmerImage(
+              //     height: size.height * 0.2,
+              //     width: double.infinity,
+              //     errorWidget: const Icon(
+              //       IconlyBold.danger,
+              //       color: Colors.red,
+              //       size: 28,
+              //     ),
+              //     imageUrl: "https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png",
+              //     boxFit: BoxFit.fill,
+              //   ),
+              // ),
+
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Title",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 17,
+                    //  fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+             
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
